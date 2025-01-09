0076226f81749910e4b68c6d7ef5fe443c8c7065
NMAtech <nadindramaulanaaziz@gmail.com>
2025-01-09 20:47:18 +0700

    init project

---
+++ src/main/java/config/env.java
@@ -0,0 +1,10 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env {
+    protected WebDriver driver;
+    protected String admin_url = "https://msib.aksamedia.com/admin-panel";
+    protected String env_username = "superadmin";
+    protected String env_password = "password";
+}
